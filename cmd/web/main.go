660b4703734836f62c70d057d3fd1b5a9ae4b7b9
Glen Barasa <glenbarasa4@gmail.com>
2026-09-11 20:19:58 +0300

    feat: implement logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infolog := log.New(os.Stdout, "INFO/t ", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR/t ", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/create", createSnippet)
@@ -14,8 +24,8 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	if err := http.ListenAndServe(":4000", mux); err != nil {
-		log.Fatal(err)
+	infolog.Printf("Starting server on %s\n", *addr)
+	if err := http.ListenAndServe(*addr, mux); err != nil {
+		errorLog.Fatal(err)
 	}
 }
