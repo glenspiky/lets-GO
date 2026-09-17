41b667e87231225bdec75aa7030a5d12abe4dd35
Glen Barasa <glenbarasa4@gmail.com>
2026-09-17 18:56:46 +0300

    feat: implement posgresql drivers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq" // To register the driver.
 )
 
 type application struct {
@@ -17,9 +20,23 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	dsn := flag.String(
+		"dsn",
+		"postgres://web:1234@localhost:5432/snippetbox?sslmode=disable",
+		"PostgreSQL data source name",
+	)
+	flag.Parse()
+
 	infolog := log.New(os.Stdout, "INFO/t ", log.Ldate|log.Ltime)
 
 	errorLog := log.New(os.Stderr, "ERROR/t ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// initialize instance of application cantainig the dependanciies
 	app := &application{
@@ -30,6 +47,19 @@ func main() {
 	srv := &http.Server{Addr: *addr, ErrorLog: errorLog, Handler: app.routes()}
 
 	infolog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/glenspiky/snippetbox
 
 go 1.27.0
+
+require github.com/lib/pq v1.12.3
